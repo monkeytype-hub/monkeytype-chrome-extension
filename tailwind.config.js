0358dc2c152d8211df1b4985346a969c247d2b0a
ridemountainpig <lin0905580802@gmail.com>
2023-03-21 02:00:50 +0800

    feat: tailwind add new css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'monkeyBlack': '#2d2e31',
+      'monkeyWhite': '#d1d0c6',
+      'monkeyYellow': '#dbba41',
+      'monkeyHoverWhite': 'rgb(241 245 249)',
+    },
   },
   plugins: [],
 }
