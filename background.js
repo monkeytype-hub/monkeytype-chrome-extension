86dca13ab431e9cb8f4d0cfc330502919b77837f
ridemountainpig <lin0905580802@gmail.com>
2023-04-02 00:06:03 +0800

    feat: add local storage function

--- background.js
+++ background.js
@@ -21,4 +21,36 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             init(tab);
         });
     }
+    if (message.type === 'storeData') {
+        const data = message.data;
+        storeData(data);
+    }
+    if (message.type === 'getData') {
+        getData(message.data, function(result) {
+            sendResponse(result);
+        });
+        return true; // This is necessary to indicate that we will be sending a response asynchronously
+    }
 });
+
+const storeData = (data) => {
+    chrome.storage.local.set(data, function () {
+        console.log(data)
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+        } else {
+            console.log('Data saved:', data);
+        }
+    });
+};
+
+const getData = (key, callback) => {
+    chrome.storage.local.get(key, function (result) {
+        if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError);
+        } else {
+            console.log('Data retrieved:', result[key]);
+            callback(result[key]);
+        }
+    });
+};
