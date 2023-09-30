476abe5b27809851d52a7ce11be09ce3248d9fdf
ridemountainpig <lin0905580802@gmail.com>
2023-09-30 22:25:30 +0800

    feat: add download monkeytype icon script

---
+++ public/script/monkeytype-icon.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const fs = require('fs');
+
+async function downloadMonkeyTypeIcons() {
+    try {
+        const response = fs.readFileSync("theme/themes.json");
+        const data = JSON.parse(response);
+
+        const downloadPromises = data.map(async (item) => {
+            const iconUrl = `https://github.com/ridemountainpig/monkeytype-icon/blob/master/monkeytype-icon/${item.name}.png?raw=true`;
+            const downloadPath = `monkeytype-icons/${item.name}.png`;
+
+            const imageResponse = await axios({
+                method: 'get',
+                url: iconUrl,
+                responseType: 'stream',
+            });
+
+            imageResponse.data.pipe(fs.createWriteStream(downloadPath));
+            console.log(`Image ${item.name}.png downloaded successfully.`);
+        });
+
+        await Promise.all(downloadPromises);
+        console.log('All images downloaded successfully.');
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+downloadMonkeyTypeIcons();
